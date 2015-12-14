321822da552e4ea35c97ea6a79fd4ecae02d2cc3
Sebastien <sdubois.sebastien@gmail.com>
2015-12-14 07:11:03 -0800

    use preference in affinity_propagation for greater accuracy

--- viz_tools.py
+++ viz_tools.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import spectral_clustering, affinity_propagation
 from sklearn.feature_extraction.text import CountVectorizer
 
-def draw_graph(data, verbose=True):
+def draw_graph(data, cluster_preference=True, verbose=True):
     df = data['df']
     labels = data['labels']
     bow_descriptor = data['bow_descriptor']
@@ -17,7 +17,6 @@ def draw_graph(data, verbose=True):
         label_tot_expenses.append(tot_expenses)
         # print labels[i],tot_expenses
     net_sizes = np.array([np.sqrt(1+abs(m)) for m in label_tot_expenses])
-    net_sizes = 20 + 10.*net_sizes
 
     # Create network
     label_co_occ = data['co_occ']
@@ -26,25 +25,18 @@ def draw_graph(data, verbose=True):
     net_labels = dict(zip(range(len(labels)),labels))
     nodelist = range(len(labels))
 
-    # movements = np.zeros((len(labels),len(labels)))
-    # edge_w = []
-    # for i,j in net.edges():
-    #     if(i!=j):
-    #         m = np.sum(abs(df[(df[labels[i]] == 1) & (df[labels[j]] == 1)]['Montant']))
-    #         m2 = np.sqrt(5+m)
-    #         movements[i,j] = m2
-    #         movements[j,i] = m2
-    #         edge_w.append(0.1*(np.log(10.+m)**2.))
-    #     else:
-    #         movements[i,j] = 1
-    #         edge_w.append(1)
-    # edge_w = 0.5*np.asarray(edge_w)
-
-    cluster_labels = affinity_propagation(label_co_occ,max_iter=500,convergence_iter=40)[1]
+    if(cluster_preference):
+        M = np.percentile(label_co_occ,75.)
+        m = np.min(label_co_occ)
+        sizeM = np.percentile(net_sizes,75.)
+        preference = m + np.asarray([min(s,sizeM) for s in net_sizes])*((M-m)/sizeM)
+        cluster_labels = affinity_propagation(label_co_occ,preference=preference, max_iter=500,convergence_iter=40)[1]        
+    else:
+        cluster_labels = affinity_propagation(label_co_occ,max_iter=500,convergence_iter=40)[1]
 
     plt.figure(figsize=(18,15))
     graph_pos = nx.spring_layout(net)
-    nx.draw_networkx_nodes(net, graph_pos, nodelist=nodelist, node_size=net_sizes, node_color=cluster_labels, alpha=0.5)
+    nx.draw_networkx_nodes(net, graph_pos, nodelist=nodelist, node_size=(20 + 10.*net_sizes), node_color=cluster_labels, alpha=0.5)
     nx.draw_networkx_edges(net, edgelist=edgelist, pos=graph_pos)
     nx.draw_networkx_labels(net, graph_pos,labels=net_labels, font_size=12, font_family='sans-serif')
     plt.show()
